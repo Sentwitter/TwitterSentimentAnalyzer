91386e1e0f9098dc12c029690bced434b770afc6
julien-leclercq <julien.leclercq@gmx.fr>
2015-09-29 14:54:15 +0200

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,30 @@
+class TweetsController < ApplicationController
+
+  def new
+    # rend le formulaire de création
+  end
+
+  def create
+    # crée la donnée (déclenchée à l'envoie du formulaire)
+  end
+
+  def index
+    # rend une liste
+  end
+
+  def show
+    # rend une donnée
+  end
+
+  def edit
+    # rend le formulaire de modification
+  end
+
+  def update
+    # modifie la donnée
+  end
+
+  def destroy
+    # détruit la / les données
+  end
+end
