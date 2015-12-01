c0a116f72262fa82e992d140e18ec53ead1a367d
Julien Leclercq <julien.leclercq@gmx.fr>
2015-12-01 09:51:06 +0100

    remove urls from tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,19 @@ class Tweet < ActiveRecord::Base
   validates :twittos, presence: true
   validates :text, presence: true, allow_nil: false
 
+  def clean
+    remove_url
+  end
+
+  URI_REGEX = %r"((?:(?:[^ :/?#]+):)(?://(?:[^ /?#]*))(?:[^ ?#]*)(?:\?(?:[^ #]*))?(?:#(?:[^ ]*))?)"
+
+  def remove_url
+    text = self.text
+    self.text = text.split(URI_REGEX).collect do |s|
+      unless s =~ URI_REGEX
+        s
+      end
+    end.join
+  end
+
 end
